affd7292caf8104edda42427b9b1852a4c24f089
Antoine POPINEAU <antoine.popineau@appscho.com>
2018-08-13 11:31:10 +0200

    Update.

--- polybar/.config/polybar/scripts/desktops.py
+++ polybar/.config/polybar/scripts/desktops.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
+import sys
+import subprocess
+
+I3_MSG='/usr/bin/i3-msg'
+
+if len(sys.argv) > 1:
+  if sys.argv[1] == 'down':
+    subprocess.run([I3_MSG, 'workspace', 'next_on_output'])
+  elif sys.argv[1] == 'up':
+    subprocess.run([I3_MSG, 'workspace', 'prev_on_output'])
+  
+  sys.exit(0)
+
 import json
 
-from subprocess import Popen, PIPE
-
-cmd = Popen(['/usr/bin/i3-msg', '-t', 'get_workspaces'], stdout=PIPE)
+cmd = subprocess.Popen([I3_MSG, '-t', 'get_workspaces'], stdout=subprocess.PIPE)
 stdout, _= cmd.communicate()
 out = json.loads(stdout)
 
