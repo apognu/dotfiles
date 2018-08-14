573871e74b05f24f0e40fdd248d0bfe73a46f3ac
Antoine POPINEAU <antoine.popineau@appscho.com>
2018-08-14 10:54:18 +0200

    Update.

--- i3/.i3/lock.sh
+++ i3/.i3/lock.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-ffmpeg -f x11grab -video_size 1920x1080 -y -i $DISPLAY -filter_complex "boxblur=2:1" -vframes 1 /tmp/lock.png
+RES=$(xdpyinfo | grep 'dimensions:' | awk '{print $2}')
+
+ffmpeg -f x11grab -y -video_size "$RES" -i "$DISPLAY" -vf 'gblur=sigma=6' -vframes 1 /tmp/lock.png
 
 SIGN_ADD_COLOR=2980b9ff
 SIGN_DEL_COLOR=e67e22ff
@@ -9,19 +11,16 @@ PASSWORD_KO=e67e22ff
 BACKGROUND=00000000
 FOREGROUND=ffffffff
 
-convert /tmp/lock.png -draw "fill rgba(0, 0, 0, 0.4) rectangle 30,970 330,1060" /tmp/lock.png
-# rectangle $CX,$CY $((CX+300)),$((CY-80)) 
-
 i3lock \
-  -t -i /tmp/lock.png \
-  --timepos="x+90:h-70" \
-  --datepos="x+135:h-45" \
-  --clock --datestr "Type password to unlock..." \
+  -i /tmp/lock.png \
+  --timepos='x+90:h-70' \
+  --datepos='x+135:h-45' \
+  --clock --datestr 'Type password to unlock...' \
   --insidecolor=$BACKGROUND --ringcolor=$FOREGROUND --line-uses-inside \
   --keyhlcolor=$SIGN_ADD_COLOR --bshlcolor=$SIGN_DEL_COLOR --separatorcolor=$BACKGROUND \
   --insidevercolor=$PASSWORD_OK --insidewrongcolor=$PASSWORD_KO \
-  --ringvercolor=$FOREGROUND --ringwrongcolor=$FOREGROUND --indpos="x+280:h-65" \
-  --radius=20 --ring-width=2 --veriftext="" --wrongtext="" \
+  --ringvercolor=$FOREGROUND --ringwrongcolor=$FOREGROUND --indpos='x+280:h-65' \
+  --radius=20 --ring-width=2 --veriftext='' --wrongtext='' \
   --verifcolor="$FOREGROUND" --timecolor="$FOREGROUND" --datecolor="$FOREGROUND" \
   --timesize=22 \
-  --noinputtext="" --force-clock $lockargs
+  --noinputtext='' --force-clock $lockargs
